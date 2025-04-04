023fcd3c07135840f1275785df5e9a0eba19664d
yoni-tad <yonitad2404@gmail.com>
2025-04-05 00:42:24 +0300

    UI change

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./index.css?v=323321";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
